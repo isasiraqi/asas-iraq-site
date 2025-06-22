cd6c814b336fa2a14292d53639b18d6a0c322985
Muntasser Hamza <isasiraqi@gmail.com>
2025-06-23 00:54:13 +0300

    وصف التعديل

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
 function goToCV() {
   window.location.href = "cv.html";
 }
-=======
-function goToCV() {
-  window.location.href = "cv.html";
-}
->>>>>>> ae19f33014a5b14f73f83abee8f8f3bb7b019455

739d440cfcf6d0ef909b3596d2d10d0920f9510a
Muntasser Hamza <isasiraqi@gmail.com>
2025-07-08 01:06:29 +0300

    وصف التعديل

--- script.js
+++ script.js
@@ -1,21 +1,5 @@
-document.querySelectorAll('.social-icon').forEach(icon => {
-  icon.addEventListener('click', function(event) {
-    event.preventDefault();
-    let url = "#";
-    switch(this.id) {
-      case "facebook":
-        url = "https://www.facebook.com/share/14G1Z7ArH2y/?mibextid=wwXIfr";
-        break;
-      case "instagram":
-        url = "https://www.instagram.com/isas.iraqi_formal_page?igsh=dnlnZWZ0ZjZ0Y2k2&utm_source=qr";
-        break;
-      case "tiktok":
-        url = "https://www.tiktok.com/@iraqiisas2025?_t=ZS-8xJRhtigRmr&_r=1";
-        break;
-      case "x":
-        url = "#"; // ضع هنا رابط تويتر متى ما أعطيتني إياه
-        break;
-    }
-    window.open(url, '_blank');
-  });
-});
+function forceOpen(event, url) {
+  event.preventDefault();
+  // الحل المختلط: اجبر حتى WebView
+  window.location.href = url;
+}
